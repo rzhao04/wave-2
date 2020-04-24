7d6dfcb0790abc43ed82c2c97f94417bc163c928
richard <richardjzzhao@gmail.com>
2020-04-23 21:26:38 -0400

    upload to wave-2

---
+++ month name to number of days.py
@@ -0,0 +1,8 @@
+month = str
+month = input ("what is month: ")
+if (month == "january" | "march" | "may" | "july" | "august" | "october" | "december"):
+    print ("31 days") 
+if (month == "april" | "june" | "september" | "november"):
+        print ("30 days")
+if (month == "feburary"):
+            print ("28 or 29 days")
