ba1d8104f17fc6cc08cc840b5a74b985626f11da
richard <richardjzzhao@gmail.com>
2020-04-24 20:54:27 -0400

    upload to wave-2

--- colour of squares.py
+++ colour of squares.py
@@ -1,12 +1,12 @@
 letter = str
 letter = raw_input ("what is letter: ")
 number = int
-number = input ("what is number: ")
-if ([letter in ["a", "c", "e", "g"] & number % 2 != 0):
+number = raw_input ("what is number: ")
+if (letter in ["a", "c", "e", "g"] and int (number) % 2 != 0):
     print ("black")
-elif ([letter in ["a", "c", "e", "g"] & number % 2 == 0):
+elif (letter in ["a", "c", "e", "g"] and int (number) % 2 == 0):
     print ("white")
-if ([letter in ["b", "d", "f", "h"] & number % 2 != 0):
+elif (letter in ["b", "d", "f", "h"] and int (number) % 2 != 0):
     print ("white")
 else:
     print ("black")
